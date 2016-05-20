c45f3880734d7ecae88ae16fee7b7f15a99cf12f
Tanmay Khanna <tk915@matrix13.doc.ic.ac.uk>
2016-05-20 16:51:19 +0100

    Created fetch function.

--- src/emulate.c
+++ src/emulate.c
@@ -1,31 +1,44 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#define INSTRUCTION_BYTE_SIZE 4
+
+int pc = 0;
+
+char* fetchInstruction(char littleEndianBuffer[]) {
+  char* instruction;
+  instruction = (char*) malloc(INSTRUCTION_BYTE_SIZE * sizeof(char));
+  for (int i = pc; i < pc + INSTRUCTION_BYTE_SIZE; i++) {
+    instruction[INSTRUCTION_BYTE_SIZE - (i - pc + 1)] = littleEndianBuffer[i];
+  }  
+  pc += 4;
+  return instruction;
+  // Need to 'free' memory occupied by instruction
+}
 
 int main(int argc, char **argv) {
-
-  FILE *fptr = fopen(argv[1], "rb");
-  fseek(fptr, 0, SEEK_END);     // setting file pointer to last place
-  int file_size = ftell(fptr);  // ftell returns the pos of the file pointer
-  rewind(fptr);           // rewind resets the file pointer to the start pos
-  unsigned char buffer[file_size];
 
   if (argc != 2) {
     printf("Expecting one argument\n");
     return EXIT_FAILURE;
   }
 
+  FILE *fptr = fopen(argv[1], "rb");
+
   if (!fptr) {
     printf("Unable to open file\n");
     return EXIT_FAILURE;
   }
 
-  fread(buffer, sizeof(buffer), 1, fptr);
-  for (int i = 0; i < sizeof(buffer); i++) {
+  fseek(fptr, 0, SEEK_END);     // setting file pointer to last place
+  int file_size = ftell(fptr);  // ftell returns the pos of the file pointer
+  rewind(fptr);           // rewind resets the file pointer to the start pos
+  unsigned char littleEndianBuffer[file_size];
+  fread(littleEndianBuffer, sizeof(littleEndianBuffer), 1, fptr);
+/*  for (int i = 0; i < sizeof(littleEndianBuffer); i++) {
     // printf omits leading zeroes by default. 02 in %02x fixes this.
-    printf("%02x", buffer[i]);
-  }
-  printf("\n");
+    printf("%02x", littleEndianBuffer[i]);
+  } */
   
   fclose(fptr);
 
