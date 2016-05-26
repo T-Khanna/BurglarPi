775d7a187ce300740868cb481349102f3eea2911
tk915 <tanmay.khanna15@imperial.ac.uk>
2016-05-26 11:32:13 +0100

    Fixed errors in b01

--- src/bitOper.h
+++ src/bitOper.h
@@ -3,7 +3,7 @@
 
 int32_t convBinToDec(int32_t bin[],int32_t size);
 int32_t* convDecToBin(int32_t num,int32_t size);
-int32_t * rotate_right(int32_t * array, int size, int rot_num);
-int32_t * shift_left(int32_t * array, int size, int rot_num);
-int32_t * shift_right(int32_t * array, int size, int rot_num);
-int32_t * arith_shift_right(int32_t * array, int size, int rot_num);
+void rotate_right(int32_t * array, int size, int rot_num);
+void shift_left(int32_t * array, int size, int rot_num);
+void shift_right(int32_t * array, int size, int rot_num);
+void arith_shift_right(int32_t * array, int size, int rot_num);
