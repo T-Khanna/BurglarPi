781632a215880f8a09cb990674bbf3e5ab84e868
Tarun Sabbineni <ts2615@imp.ac.uk>
2016-05-30 20:53:18 +0100

    Finished writing. Need to fix compilation errors

--- src/gpio_helper.c
+++ src/gpio_helper.c
@@ -7,13 +7,15 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #include "ARMgen.h"
+#include "gpio_helper.h"
+#include <stdio.h>
 
+#define GPIO_ADDR_0_9			      0x20200000
+#define GPIO_ADDR_10_19		    	0x20200004
+#define GPIO_ADDR_20_29			    0x20200008
+#define GPIO_ADDR_SET		  	    0x2020001C
+#define GPIO_ADDR_CLEAR	    		0x20200028
 
-//-- FUNCTION DECLARATIONS -----------------------------------------------------
-
-void setGPIOAddr();
-void clearGPIOAddr();
-int convGPIOAddr();
 
 
 //-- FUNCTIONS -----------------------------------------------------------------
@@ -21,9 +23,9 @@ int convGPIOAddr();
 //sets addresses as the values of the GPIO pins
 void setGPIOAddr(current_state* cur_state) {
   
-	cur_state->memory[MEMORY_CAPACITY - 1] = 0x20200008;
-	cur_state->memory[MEMORY_CAPACITY - 2] = 0x20200004;
-	cur_state->memory[MEMORY_CAPACITY - 3] = 0x20200000;
+	cur_state->memory[MEMORY_CAPACITY - 3] = GPIO_ADDR_0_9;
+	cur_state->memory[MEMORY_CAPACITY - 2] = GPIO_ADDR_10_19;
+	cur_state->memory[MEMORY_CAPACITY - 1] = GPIO_ADDR_20_29;
 
 }
 
@@ -37,9 +39,42 @@ void clearGPIOAddr(current_state* cur_state) {
 }
 
 //converts address of GPIO pins to last memory addresses
-int convGPIOAddr() {
-  
-  return 0;
+int convGPIOAddr(int* addr) {
+
+  switch(*addr) {
+
+    //SET PIN ADDRESS
+    case GPIO_ADDR_SET:
+      printf("PIN ON\n");
+      return 1;
+
+		//CLEAR PIN ADDRESS
+		case GPIO_ADDR_CLEAR:
+			printf("PIN OFF\n");
+			return 1;
+
+    //GPIO ports 0-9 accesssed
+    case GPIO_ADDR_0_9:
+      printf("One GPIO pin from 0 to 9 has been accessed\n");
+      break;
+
+		//GPIO ports 10-19 accesssed
+		case GPIO_ADDR_10_19:
+			printf("One GPIO pin from 10 to 19 has been accessed\n");
+			break;
+    
+		//GPIO ports 20-29 accesssed
+		case GPIO_ADDR_20_29:
+    	printf("One GPIO pin from 20 to 29 has been accessed\n");
+			break;
+
+		default:
+			return 0;
+
+	}
+
+	*addr = ((MEMORY_CAPACITY - ((*addr - GPIO_ADDR_10_19) / 4)) - 1) * 4;
+	return 0;
 }
 
 
---
+++ src/gpio_helper.h
@@ -0,0 +1,23 @@
+////////////////////////////////////////////////////////////////////////////////
+// ARM Group Project - Year 1 (Group 40)
+// _____________________________________________________________________________
+// 
+// File: execute_helper.c
+// Members: Tarun Sabbineni, Vinamra Agrawal, Tanmay Khanna, Balint Babik
+////////////////////////////////////////////////////////////////////////////////
+
+
+//------------------------ EMULATE HELPER HEADER -------------------------------
+// Contains the function definitions for emulate.c, the execute function in 
+// particular to carry out the four instructions specified.
+
+
+
+//-- FUNCTION DECLARATIONS -----------------------------------------------------
+
+void setGPIOAddr();
+void clearGPIOAddr();
+int convGPIOAddr();
+
+
+
