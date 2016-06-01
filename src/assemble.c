939afd395ac81976823bb0c5395871d229c54650
Tarun Sabbineni <ts2615@imp.ac.uk>
2016-06-01 11:26:17 +0100

    Recovered yesterday's helper function main code

--- src/assemble.c
+++ src/assemble.c
@@ -14,15 +14,16 @@
 
 //-- FUNCTION DECLARATIONS ----------------------------------------------------
 
-int32_t* get_instr(char* path);
+char* get_instr(char* path);
 void tokeniser(char* line);
-int32_t* translate_instr(int32_t* assem_instr);
+int32_t* translate_instr(char* assem_instr);
 void write_bin(char* path, int32_t* bin_instr);
 
 
 //-- GLOBAL VARIABLES ---------------------------------------------------------
 
 struct symbol_table symb_table;
+int file_size;
 
 
 //-- MAIN ---------------------------------------------------------------------
@@ -37,7 +38,7 @@ int main(int argc, char **argv) {
 
   //getting the instruction from source file into an array of 32-bit
   //instructions that will be translated.
-  int32_t* assem_instr = get_instr(argv[1]);
+  char* assem_instr = get_instr(argv[1]);
 
   //performing the pass over the file to decode into binary that will be written
   int32_t* bin_instr = translate_instr(assem_instr);
@@ -53,7 +54,7 @@ int main(int argc, char **argv) {
 //-- FUNCTION DEFINTIONS -------------------------------------------------------
 
 //gets instructions from source file into an array of 32-bit instructions
-int32_t* get_instr(char* path) {
+char* get_instr(char* path) {
 
   // Open source assembly file
   FILE *fptr = fopen(path, "r");
@@ -67,19 +68,35 @@ int32_t* get_instr(char* path) {
   //TODO: CODE that turns each instruction in the source file into an array
   //      of 32-bit instructions.
 
-  char line[LIMIT_PER_LINE];
-  
-  // Better than !feof(fptr) as it actually stops at EOF 
-  while (fgets(line, LIMIT_PER_LINE, fptr)) {
-    // This removes the trailing '\n' from fgets
-    line[strcspn(line, "\n")] = '\0';
-    puts(line);
-    tokeniser(line);
+  //Vini's code 
+  // char line[LIMIT_PER_LINE];
+  // // Better than !feof(fptr) as it actually stops at EOF 
+  // while (fgets(line, LIMIT_PER_LINE, fptr)) {
+  //   // This removes the trailing '\n' from fgets
+  //   line[strcspn(line, "\n")] = '\0';
+  //   puts(line);
+  //   tokeniser(line);
+  // }
+ 
+ 
+  //setting file point32_ter to the end of the file
+  fseek(fptr, 0, SEEK_END);         
+
+  //ftell returns the pos of the file point32_ter
+  int file_size = ftell(fptr);	
+  rewind(fptr); 
+
+  //coverting from num of bytes to num of 32bit word instructions
+  file_size /= INSTRUCTION_BYTE_SIZE;
+
+  //saving the file data in the array
+  char* instructions[file_size];
+  for(int i = 0; i < file_size; i++){
+    fread(&(instructions[i]), sizeof(int32_t), 1, fptr);
   }
   
-
   fclose(fptr);
-  return NULL;
+  return *(instructions);
 
 }
 
@@ -107,9 +124,24 @@ void tokeniser(char *line) {
 
 
 //return an array of 32 bit words to be written into binary file
-int32_t* translate_instr(int32_t* assem_instr) {
+int32_t* translate_instr(char* assem_instr) {
 
   //TODO: CODE that translates each 32 bit word into binary
+  //while(not end of file) {
+  //  currentInstruction = (correct 32bit int in assem_instr array/pointer);
+  //  tokenLine = tokenise(currentInstruction);
+  //  binLine = commandProcessing(tokenLine);
+  //  append(bin instrs, binline);
+  //  go to next line i++
+  //}
+
+  //i = 0;
+  //while(i < file_size){
+
+  //  int32_t* currentInstruction;
+  //  
+  //  
+  //}
 
   return NULL;
 
@@ -122,10 +154,16 @@ void write_bin(char *path, int32_t* bin_instr) {
   // Creating output binary file
   FILE *fptr = fopen(path, "w+b");
 
-  //TODO: CODE that writes given binary to the file specified
+  for(int i = 0; i < file_size; i++){
+    fwrite((bin_instr + i * INSTRUCTION_BIT_SIZE), 1, 
+           INSTRUCTION_BIT_SIZE, fptr);
+    fputs("\n", fptr);
+  }
 
+  //closing file
   fclose(fptr);
   
 }
+
 
 
