0fef6f739f4130d2cf3ef1d3a9e0bb3d0192d8d8
tk915 <tanmay.khanna15@imperial.ac.uk>
2016-06-04 11:13:50 +0100

    Fixed small error where bitOper.c was #included instead of bitOper.h

--- src/OPFunctions.c
+++ src/OPFunctions.c
@@ -10,7 +10,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "ARMasm.h"
-#include "bitOper.c"
+#include "bitOper.h"
 
 //-- FUNCTION DECLARATIONS ----------------------------------------------------
 
