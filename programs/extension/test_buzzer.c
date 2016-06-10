d53d8215ce1e52ed3bc1853178c8e1824738e34c
Vinamra Agrawal <Vini@dyn1206-36.wlan.ic.ac.uk>
2016-06-10 04:35:26 +0100

    removed commented lines

--- programs/extension/test_buzzer.c
+++ programs/extension/test_buzzer.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-//#include<wiringPi.h>
+#include<wiringPi.h>
 
 #define PIRPIN  17
 #define REDPIN  18
@@ -34,7 +34,7 @@ int introductionMenu(){
 
    printf("Developed by Balint Babik, Tanmay Khanna, Tarun Sabbinieni and Vinamra Agrawal\n\n\n\n");
 
-   //delay(1000);
+   delay(1000);
 
    printf("Pease enter your password \n");
 
@@ -126,8 +126,8 @@ void executeOption(int opt){
       case 4:printLog();
              break;
       case 5:printf("exiting\n");
-             //digitalWrite(REDPIN, LOW);
-             //digitalWrite(BLUEPIN, LOW);
+             digitalWrite(REDPIN, LOW);
+             digitalWrite(BLUEPIN, LOW);
              exit(0);
              break;
       default: printf("Invalid option given");
@@ -168,9 +168,9 @@ void printSettings(){
                 fwrite(password, sizeof(password), 1, fptr);
                 fclose(fptr);
                 printf("password changes successfully\n");
-                //delay(1000);
+                delay(1000);
                 printf("Returning to main menu\n");
-                //delay(500);
+                delay(500);
                 executeOption(printMainMenu());
               }
               break;
@@ -207,10 +207,10 @@ void printSettings(){
                   fclose(fptr);
                
                 printf("email changes successfully\n");
-                //delay(1000);
+                delay(1000);
                 }
                 printf("Returning to main menu\n");
-                //delay(500);
+                delay(500);
                 free(email);
                 executeOption(printMainMenu());
                break; 
@@ -230,27 +230,27 @@ void run_alarm(int delay_time, int loop_nums,int state) {
   
  
   // setting all pins to 0
-  //wiringPiSetupGpio();
+  wiringPiSetupGpio();
 
   // setting up output pins
-  //pinMode(REDPIN, OUTPUT);
-  //pinMode(BLUEPIN, OUTPUT);
-  //pinMode(BUZZER, OUTPUT);
+  pinMode(REDPIN, OUTPUT);
+  pinMode(BLUEPIN, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
   
   // setting up input pin
-  //pinMode(PIRPIN, INPUT);
+  pinMode(PIRPIN, INPUT);
   
   printf("Running alarm, current mode is:");
   
   // displaying mode setting
   if (state == ON) {
     printf("ON\n");
-    //digitalWrite(REDPIN, HIGH);
-    //digitalWrite(BLUEPIN, LOW);
+    digitalWrite(REDPIN, HIGH);
+    digitalWrite(BLUEPIN, LOW);
   } else {
     printf("OFF\n");
-    //digitalWrite(REDPIN, LOW);
-    //digitalWrite(BLUEPIN, HIGH);
+    digitalWrite(REDPIN, LOW);
+    digitalWrite(BLUEPIN, HIGH);
   }
   
   int curr_state = 0;
@@ -258,33 +258,33 @@ void run_alarm(int delay_time, int loop_nums,int state) {
 
   // settling
   printf("waiting for PIR to settle...\n");
-  while (1){//digitalRead(PIRPIN) == 1) {
+  while (digitalRead(PIRPIN) == 1) {
   }
   printf("ready\n");
 
   if (state == ON) {
     // service loop
     for(int i = 0; i < loop_nums; i++) {
-      curr_state = 0;//digitalRead(PIRPIN);
+      curr_state = digitalRead(PIRPIN);
       printf("%i\n", curr_state);
       if ((curr_state == 1) && (prev_state == 0)) {
-        //digitalWrite(BUZZER, HIGH); 
+        digitalWrite(BUZZER, HIGH); 
         printf("I see you! .. Baka!\n");
         prev_state = 1;
       } else if((curr_state == 0) && (prev_state == 1)) {
-        //digitalWrite(BUZZER, LOW);
+        digitalWrite(BUZZER, LOW);
         printf("Ready\n");
         prev_state = 0;       
       }
       
-      //delay(delay_time);
-      //digitalWrite(BUZZER, LOW);
+      delay(delay_time);
+      digitalWrite(BUZZER, LOW);
     }   
   }
   
   printf("Setting alarm off\n");
-  //digitalWrite(REDPIN, LOW);
-  //digitalWrite(BLUEPIN, LOW);
+  digitalWrite(REDPIN, LOW);
+  digitalWrite(BLUEPIN, LOW);
 }
 
 int main(void){
@@ -303,7 +303,7 @@ int main(void){
 
  executeOption(menuOpt);
 
- //run_alarm(1000, 22, alarmState);
+ 
  return 0;
 } else {
 
